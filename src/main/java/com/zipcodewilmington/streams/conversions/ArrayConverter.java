4af8fd2c266228a27f4d3f0510488e5b37c07032
Greg Davis <jrobley3@hotmail.com>
2020-03-28 17:43:17 -0400

    array converter done

--- src/main/java/com/zipcodewilmington/streams/conversions/ArrayConverter.java
+++ src/main/java/com/zipcodewilmington/streams/conversions/ArrayConverter.java
@@ -3,6 +3,7 @@ package com.zipcodewilmington.streams.conversions;
 import com.zipcodewilmington.streams.anthropoid.Person;
 import com.zipcodewilmington.streams.anthropoid.PersonFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,16 +26,16 @@ public final class ArrayConverter extends PersonConversionAgent<Person[]> {
 
     //TODO
     public List<Person> toList() {
-        return null;
+        return new ArrayList<>();
     }
 
     //TODO
     public Stream<Person> toStream() {
-        return null;
+        return toList().stream();
     }
 
     @Override
     public Person[] toArray() {
-        return super.objectSequence;
+        return toStream().toArray(Person[]::new);
     }
 }
